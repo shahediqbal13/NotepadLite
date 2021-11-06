ee547b046513b0d6ba4007929b75cb5533c953ff
Shahed Iqbal <shahediqbal13@gmail.com>
2021-11-06 15:07:06 +0600

    Notepad Lite base skeleton

---
+++ NotepadLite/NotepadLite/View/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotepadLite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(MainWindow));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Log.Info("Log is working!");
+        }
+
+        private void OnNewMenuClicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("New");
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OnOpenMenuClicked(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
